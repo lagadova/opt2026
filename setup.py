cee21ad77225783cf18ee26cb71848aa7f887fdf
Ладелаида Лагадова <ladelaida.lagadova@gmail.com>
2026-02-21 21:04:31 +0100

    add fork

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 import qrcode
 import yaml
 
-
 def create_qr_code(url: str):
     qr = qrcode.QRCode(
-        version=1,
+       version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
-        border=4,
+       border=4,
     )
 
     qr.add_data(url)
     qr.make()
-    img = qr.make_image(fill_color="black", back_color="white")
+    img = qr.make_image(fill_color="purple", back_color="white")
 
     return img
 
